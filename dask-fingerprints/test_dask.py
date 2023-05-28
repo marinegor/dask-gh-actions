798d056f708f00d03b1ca433a98428e060815a83
Egor Marin <marin@phystech.edu>
2023-05-28 22:18:23 +0300

    Modify tests using a conftest project-wide fixture

--- dask-fingerprints/test_dask.py
+++ dask-fingerprints/test_dask.py
@@ -11,47 +11,20 @@ def trajectory_single_frame():
 
 
 @pytest.fixture
-def trajectory_many_frames(client):
+def trajectory_many_frames():
     return simulate_trajectory([1, 2, 3], length=10_000)
 
 
-# @pytest.fixture
-# def dask_client_with_maximum_workers_as_threads():
-# from dask.distributed import Client
-# import multiprocessing
-#
-# client = Client(n_workers=multiprocessing.cpu_count(), processes=False)
-# return client
-
-
-def test_worker_pid_is_not_the_same():
-    from dask.distributed import Client
-
-    _ = Client(n_workers=1)
+@pytest.mark.parametrize("scheduler", ["distributed", "processes"], indirect=True)
+def test_worker_pid_is_not_the_same(scheduler):
     current_pid = getpid()
     work_pid = worker_pid().compute()
+    assert current_pid != work_pid, scheduler
 
-    assert current_pid != work_pid, (current_pid, work_pid)
 
-
-def test_pids_are_same_if_use_threads():
-    from dask.distributed import Client
-
-    _ = Client(n_workers=1, processes=False)
+@pytest.mark.parametrize("scheduler", ["synchronous"], indirect=True)
+def test_pids_are_same_if_use_threads(scheduler):
     current_pid = getpid()
     work_pid = worker_pid().compute()
 
     assert current_pid == work_pid, (current_pid, work_pid)
-
-
-def test_number_of_pids_equals_number_of_workers():
-    from dask.distributed import Client
-
-    ncpu = multiprocessing.cpu_count()
-    _ = Client(n_workers=ncpu)
-    current_pid = getpid()
-    worker_pids = [worker_pid().compute() for _ in range(50)]
-    worker_pids = set(worker_pids)
-
-    assert current_pid not in worker_pids, (current_pid, worker_pids)
-    assert len(worker_pids) == ncpu, (worker_pids, ncpu)
